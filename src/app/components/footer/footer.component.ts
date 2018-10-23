0494ff50cafeced02d3d11e3dec2e5f65504db7b
benkrig <benjaminjkrig@gmail.com>
2018-10-23 12:13:27 -0700

    update footer and app

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
 
+  constructor () {}
 
+  // noinspection JSMethodCanBeStatic
+  openGithub() {
+    window.open('https://github.com/SE-131-JSONx', '_blank');
+  }
 }
