e490b8db5587d9d404767688fb1820099c17b8aa
Salvatore Nicosia <nicosia.salvo24@gmail.com>
2018-11-01 19:59:55 -0700

    Added the backend connection to get the total number of json and member of count. Now the dashboard shows the current number correctly. Shared JSON and Owner need backend connection.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Profile, ProfileInit} from '../../util/interfaces';
+import {BackendService} from '../../services/backend.service';
+import {JwtHelperService} from '@auth0/angular-jwt';
+import {globals} from '../../util/globals';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  profile: Profile = ProfileInit();
 
-  constructor() { }
+  constructor(private backendService: BackendService) {
+    const helper = new JwtHelperService();
+    const decodedToken = helper.decodeToken(globals.JWT);
+    const user_id = decodedToken ? decodedToken.user_id : null;
+
+    this.backendService.getUserDetails(user_id, (error, r) => {
+      this.profile = <Profile> r;
+    });
+  }
 
   ngOnInit() {
   }
-
 }
